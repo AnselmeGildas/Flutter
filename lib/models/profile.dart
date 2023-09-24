c3121536c2f5d7c181dbd032549b8989771910e8
kadioanselme <kadioanselme@gmail.com>
2023-09-24 11:44:23 +0000

    message

---
+++ lib/models/profile.dart
@@ -0,0 +1,25 @@
+class Profile {
+  Profile({
+    required this.id,
+    required this.username,
+    required this.number,
+    required this.createdAt,
+  });
+
+  /// User ID of the profile
+  final String id;
+
+  /// Username of the profile
+  final String username;
+
+  /// number of the profile
+  final String number;
+
+  /// Date and time when the profile was created
+  final DateTime createdAt;
+
+  Profile.fromMap(Map<String, dynamic> map)
+      : id = map['id'],
+        username = map['username'],
+        createdAt = DateTime.parse(map['created_at']);
+}
